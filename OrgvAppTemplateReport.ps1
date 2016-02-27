a39ca54d546f0c2466659ce2d95a73739f4fa6fe
caaxx <cathalcleary@gmail.com>
2016-02-27 13:01:47 +0000

    Initial commit
    Query for all vAppTemplates in an Org

---
+++ OrgvAppTemplateReport.ps1
@@ -0,0 +1,8 @@
+﻿# Use Search-Cloud to find all AdminVMs and filter to see if they are vApp Templates. This is the part I had a hard time with using the Get-* commandlets.
+search-cloud -querytype adminvapptemplate -filter "org==$((get-org GSS-SRT).id)" | `
+     select @{N='Catalog';               E={ $_.CatalogName} }, `
+            @{N='VM Name';               E={ $_.Name} }, `
+            @{N='Creation Date';         E={ $_.CreationDate} }, `
+            @{N='VDC Name';              E={ $_.VdcName} }, `
+            @{N='OwnerName';             E={ $_.OwnerName }}` `
+     | export-csv core5-srt-templates.csv
